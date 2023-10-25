24fcf6cf92885083dc05db92b40aabe969649760
Alisherjon <egamberdiyevalisherjon@gmail.com>
2023-10-25 10:23:25 +0500

    array methods

--- js/app.js
+++ js/app.js
@@ -23,3 +23,33 @@
 // console.log(str.trim());
 // console.log(str.trimStart());
 // console.log(str.trimEnd());
+
+// Array methods
+
+let fruits = ["Apple", "Lime", "Banana", "Cherry", "Strawberry", "Water melon"];
+
+// console.log(fruits.length);
+// console.log(fruits.concat(["Pear", "Lime"], ["Strawberry", "Water melon", ["Pineapple"]]));
+// console.log(fruits.flat(2));
+// console.log(fruits.includes("apple"));
+// console.log(fruits.indexOf("Apple"));
+// console.log(fruits.lastIndexOf("Apple"));
+// console.log(fruits.lastIndexOf("Apple"));
+// console.log(fruits.join(" -> "));
+// console.log(fruits.reverse());
+// console.log("fruits".split("").reverse().join(""));
+// console.log(fruits.slice(1, 3));
+// console.log(fruits.sort());
+// console.log(fruits.sort().reverse());
+
+// fruits.push("Strawberry");
+// fruits.unshift("Strawberry");
+// fruits.pop();
+// fruits.shift();
+// fruits.splice(2, 0, "Pear", "Pineapple");
+// fruits.splice(fruits.length, 0, "Pear"); // push
+// fruits.splice(0, 0, "Pear"); // unshift
+// fruits.splice(fruits.length - 1, 1); // pop
+// fruits.splice(0, 1); // shift
+
+// console.log(fruits);
